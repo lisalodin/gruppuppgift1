aa4924756ddbac010ec723074b4fde0560f220a7
Cecilia Edstein <cecilia.edstein@gmail.com>
2025-11-12 15:33:18 +0100

    finished

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./style.css";
-const BASE_URL = "https://www.omdbapi.com/?apikey=9eae2121&s=harry;";
+const BASE_URL = "https://www.omdbapi.com/?apikey=9eae2121&";
 
 const get = async <T>(URL: string) => {
   const response = await fetch(URL);
@@ -7,6 +7,31 @@ const get = async <T>(URL: string) => {
   return data;
 };
 
+export const createHtml = (movies: Movie[]) => {
+  const moviesSection = document.getElementById("movies");
+
+  if (moviesSection) {
+    moviesSection.innerHTML = "";
+  }
+
+  movies.forEach((movie) => {
+    const movieContainer = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const img = document.createElement("img");
+    const title = document.createElement("h2");
+    img.src = movie.Poster;
+    title.innerHTML = movie.Title;
+
+    movieContainer.addEventListener("click", async () => {
+      const movieDetails = await getMovieById(movie.imdbID);
+    });
+
+    imgContainer.appendChild(img);
+    movieContainer.appendChild(title);
+    movieContainer.appendChild(imgContainer);
+    moviesSection?.appendChild(movieContainer);
+  });
+};
 
 export type OmdbResponse = {
   Search: Movie[];
@@ -17,6 +42,7 @@ export type Movie = {
   Poster: string;
   imdbID: string;
 };
+
 export const getMovies = async (searchText: string) => {
   const response = await get<OmdbResponse>(`${BASE_URL}s=${searchText}`);
   return response.Search;
@@ -26,12 +52,7 @@ export const getMovieById = async (id: string) => {
   return await get<Movie>(`${BASE_URL}i=${id}`);
 };
 
-
-window.addEventListener("DOMContentLoaded", () => {
 const app = document.getElementById("app") as HTMLElement;
-
-const form = document.createElement("form");
-form.id = "searchForm";
 
 const input = document.createElement("input");
 input.type = "text";
@@ -39,6 +60,15 @@ input.id = "searchText";
 
 const button = document.createElement("button");
 button.textContent = "Sök";
+
+const form = document.createElement("form");
+form.id = "searchForm";
+// form.addEventListener("submit", async (e) => {
+//   e.preventDefault();
+
+//   const movies = await getMovies(input.value);
+//   createHtml(movies);
+// });
 
 form.appendChild(input);
 form.appendChild(button);
@@ -48,4 +78,21 @@ movies.id = "movies";
 
 app.appendChild(form);
 app.appendChild(movies);
+
+document.getElementById("searchForm")?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const theInput = document.getElementById("searchText");
+
+  let searchText = "";
+  if (theInput) {
+    searchText = (theInput as HTMLInputElement).value;
+  }
+
+  const movies = await getMovies(searchText);
+  createHtml(movies);
+
+  if (theInput) {
+    (theInput as HTMLInputElement).value = "";
+  }
 });
