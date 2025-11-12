6688db48d960ace26a1caeee4294d2509d6ec2e8
Cecilia Edstein <cecilia.edstein@gmail.com>
2025-11-12 13:17:10 +0100

    fetch

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,11 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+const BASE_URL = "https://www.omdbapi.com/?apikey=9eae2121&s=harry;";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+export const getMovies = async (searchText: string) => {
+  const response = await get<OmdbResponse>(`${BASE_URL}s=${searchText}`);
+  return response.Search;
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+export const getMovieById = async (id: string) => {
+  return await get<Movie>(`${BASE_URL}i=${id}`);
+};
